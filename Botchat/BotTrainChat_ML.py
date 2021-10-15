631a23eccff90d280105d9f4df2d653d9a3f41f8
ThatsadaphoneITPD <59547857+ThatsadaphoneITPD@users.noreply.github.com>
2021-10-15 22:13:36 +0700

    Add file Smart
    create Chatbot by ML and Neural network to predict correct of complex word pattern

---
+++ Botchat/BotTrainChat_ML.py
@@ -0,0 +1,71 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+import pickle
+import numpy as np
+import json
+import random
+
+lemmatizer = WordNetLemmatizer()
+
+model = load_model('chatbot_model.h5')
+
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - splitting words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stemming every word - reducing to base form
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+# return bag of words array: 0 or 1 for words that exist in sentence
+def bag_of_words(sentence, show_details=True):
+    # tokenizing patterns
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,word in enumerate(words):
+            if word == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                # this line use to check
+                # how bot get input word data to find match answer
+                # with trained Data by DL
+                # you can select those two line then Ctrl+/
+                if show_details:
+                    print("found in bag: %s" % word)
+    return np.array(bag)
+
+def predict_class(sentence):
+    # filter below  threshold predictions
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sorting strength probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+#check on run and terminal
+while True:
+    message = input("You: ")
+    ints = predict_class(message)
+    res = getResponse(ints, intents)
+    print("MeiBot: {}".format(res))
